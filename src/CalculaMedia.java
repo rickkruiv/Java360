5f432f5f5576dabe0bfa28c427788159ee2e610a
Henrique Ferreira Polizer <polizerrickk@gmail.com>
2025-02-06 15:40:10 -0300

    Java360

---
+++ src/CalculaMedia.java
@@ -0,0 +1,40 @@
+/******************************************************************************/
+/* Autor...: Henrique Ferreira Polizer
+/* Data....: 05-02-2025
+/* Funcao..: Calcular a média de um aluno com 3 notas                          
+/******************************************************************************/
+public class CalculaMedia {
+   
+   public static void main( String[] args ) {
+
+      var scanner = new java.util.Scanner( System.in );
+      String situacao;
+
+      System.out.print( "Nome do Aluno: " );
+      String nomeAluno = scanner.nextLine();
+
+      System.out.print( "Primeira nota: " );
+      double nota1 = Double.parseDouble( scanner.nextLine() );
+
+      System.out.print( "Segunda nota: " );
+      double nota2 = Double.parseDouble( scanner.nextLine() );
+
+      System.out.print( "Terceira nota: " );
+      double nota3 = Double.parseDouble( scanner.nextLine() );
+
+      double media = ( nota1 + nota2 + nota3 ) / 3;
+
+      if ( media >= 6.0 || media >= 60.0 ) {
+         situacao = "Aprovado";
+      } else if ( media >= 4.0 || media >= 40.0 ) {
+         situacao = "Dependencia";
+      } else {
+         situacao = "Reprovaddo";
+      }
+
+      System.out.println( "O aluno " + nomeAluno + " esta " + situacao + " e sua média é de " + String.format( "%.1f", media) );
+      scanner.close();
+
+   }
+
+}
---
+++ src/ImparOuPar.java
@@ -0,0 +1,35 @@
+public class ImparOuPar {
+   
+   public static void main( String[] args) {
+
+      int    nValor;
+      var    scanner      = new java.util.Scanner( System.in );
+      String cValorString = "";
+      String cParidade    = ""; 
+
+      while ( true ) {
+         System.out.print( "Informe um valor para verificação: " );
+         cValorString = scanner.nextLine();
+
+         try {
+            nValor = Integer.parseInt( cValorString );
+            break;
+         } catch( NumberFormatException e ) {
+            System.out.println( "\u001B[31mErro: Entrada inválida. Digite apenas números inteiros!\u001B[0m" );
+         }
+      }
+
+      if ( nValor % 2 == 0 ) {
+         cParidade = "par";
+         nValor += 5;
+      } else {
+         cParidade = "impar";
+         nValor += 2;
+      }
+
+      System.out.println( "O número digitado é " + cParidade );
+      System.out.println( "O novo valor é " + nValor );
+      scanner.close();
+
+   }
+}
