9b7dfbf94ef48e9e8c3dbd3259186658ce697ac6
Henrique Ferreira Polizer <polizerrickk@gmail.com>
2025-02-10 16:47:13 -0300

    feat: Entendendo sobre métodos e atributos static

---
+++ src/App07.java
@@ -0,0 +1,21 @@
+public class App07 {
+
+   public static void main(String[] args) {
+      
+      IdGeneratedAccount idGenAccount1 = new IdGeneratedAccount("Henrique");
+      System.out.println(idGenAccount1.getAccountNumber());
+      
+      IdGeneratedAccount idGenAccount2 = new IdGeneratedAccount("Henrique");
+      System.out.println(idGenAccount2.getAccountNumber());
+      
+      IdGeneratedAccount idGenAccount3 = new IdGeneratedAccount("Henrique");
+      System.out.println(idGenAccount3.getAccountNumber());
+      
+      IdGeneratedAccount idGenAccount4 = new IdGeneratedAccount("Henrique");
+      System.out.println(idGenAccount4.getAccountNumber());
+
+      System.out.println(IdGeneratedAccount.getCurrentId());
+
+   }
+}
+
