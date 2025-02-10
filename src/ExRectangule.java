6a3de33f4d104a52dae39f336c9a4300c5947c3b
Henrique Ferreira Polizer <polizerrickk@gmail.com>
2025-02-10 11:00:37 -0300

    feat: Exercícios sobre construtores e encapsulamento - Implementa a classe Lamp com métodos para ligar, desligar e exibir estado. - Adiciona a classe Rectangule com sobrecarga de construtores para representar um retângulo e calcular sua área. - Inclui classes AppLamp e ExRectangule para testar as funcionalidades.

--- src/ExRectangule.java
+++ src/ExRectangule.java
@@ -1,5 +1,12 @@
+/******************************************************************************/
+/*
+ * Autor...: Henrique Ferreira Polizer
+ * /* Data....: Data
+ * /* Funcao..: Funcao
+ * /
+ ******************************************************************************/
 public class ExRectangule {
-   
+
    public static void main(String[] args) {
       Rectangule rectangule = new Rectangule(8, 10);
       System.out.printf("Área do retâgulo: %.2f\n", rectangule.square());
